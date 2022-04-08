d405a6afc47c956407dc7c8412f25d0ec73d018d
Raphael <raphaelrcbarbosa@gmail.com>
2022-04-08 01:06:03 -0300

    file for register page

---
+++ src/pages/Register/RegisterUser.js
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterBox from "components/Register/Register";
+
+const PagesRegisterUser = () => {
+    return (
+        <RegisterBox />
+    );
+};
+
+export default PagesRegisterUser;
