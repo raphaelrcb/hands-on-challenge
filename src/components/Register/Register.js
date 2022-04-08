aa98157cd1803aa2f3c6989f1fc0bc1997dc3d25
Raphael <raphaelrcbarbosa@gmail.com>
2022-04-08 02:47:11 -0300

    Created input boxes and state changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,89 @@
 import React from 'react';
+import UseState from 'react';
 
-const RegisterBox = () => (
-    <div>Register</div>
-)
+class RegisterBox extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+            username: '',
+            password: '',
+            confirm: ''
+        
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(){
+        console.log(this.state)
+    }    
+
+    render () {
+        return (
+            <div>
+                <div className='inner-container'>
+                    <div className='register-header'>
+                        <h1>Register</h1>
+                    </div>
+                    <div className='register-box'>
+
+                        <div className='input-group'>
+                            <label htmlFor='username'>Username</label>
+                            <input
+                                type = 'text'
+                                name = 'username'
+                                className='register-input'
+                                placeholder='Username' 
+                                value={this.state.username}
+                                onChange={this.handleChange}/>
+                        </div>
+
+                        <div className='input-group'>
+                            <label htmlFor='password'>Password</label>
+                            <input
+                                type = 'password'
+                                name = 'password'
+                                className='register-input'
+                                placeholder='********'
+                                value={this.state.password}
+                                onChange={this.handleChange}/>
+                        </div>
+
+                        <div className='input-group'>
+                            <label htmlFor='password'>Confirm Password</label>
+                            <input
+                                type = 'password'
+                                name = 'confirm'
+                                className='register-input'
+                                placeholder='********'
+                                value={this.state.confirm}
+                                onChange={this.handleChange}/>
+                        </div>
+
+                        <button
+                            type='button'
+                            className='register-button'
+                            onClick={this.handleSubmit}> Registrar
+                        </button>
+
+
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+    
+}
 
 export default RegisterBox;
