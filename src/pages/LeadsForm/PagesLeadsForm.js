3ab60386975a1d89a30d91ef0da6989eddd663cf
Raphael <raphaelrcbarbosa@gmail.com>
2022-04-09 04:20:27 -0300

    Created LeadsForm

---
+++ src/pages/LeadsForm/PagesLeadsForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import LeadsPanel from "components/Leads/LeadsPanel"
+
+const PagesLeadsForm = () => {
+    return (
+        <div>
+            FORMULARIO
+        </div>  
+    );
+};
+
+export default PagesLeadsForm;
