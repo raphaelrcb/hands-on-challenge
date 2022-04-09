9b8f425e96645f84c0d5ea08464b527bf4406c30
Raphael <raphaelrcbarbosa@gmail.com>
2022-04-09 03:25:21 -0300

    Created Page for Lead Panel

---
+++ src/pages/LeadPanel/LeadPanel.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PagesLeadPanel = () => {
+    return (
+        <div>
+            Lead Panel
+        </div>
+        
+    );
+};
+
+export default PagesLeadPanel;
