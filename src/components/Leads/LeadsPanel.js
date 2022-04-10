0a42bb47f66b710504c83ba85723790ef64ebc67
Raphael <raphaelrcbarbosa@gmail.com>
2022-04-10 17:22:08 -0300

    minor changes

--- src/components/Leads/LeadsPanel.js
+++ src/components/Leads/LeadsPanel.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logoImg from "../../logo.jpg";
 import { Link } from 'react-router-dom';
 import "./LeadsPanel.css"
 
 const LeadsPanel = () => {
+    
+    // const [newLead, setNewLead] = useState([])
+    // function getNewLead () {
+    //     setNewLead(JSON.parse(localStorage.getItem("new_lead")))
+    //     return newLead
+    // }
+
 
     return(
-        <div className='header-box'>
-            <header className='leads-header'>
-                <img src= {logoImg} className='leads-img-header' alt="Elogroup logo" />
-                <h1> Lead Panel </h1>
-                <Link to="/newlead" className='new-lead-link'> New Lead (+) </Link>
-            </header>
+        <div>
+            <div className='header-box'>
+                <header className='leads-header'>
+                    <img src= {logoImg} className='leads-img-header' alt="Elogroup logo" />
+                    <h1> Lead Panel </h1>
+                    <Link to="/newlead" className='new-lead-link'> New Lead (+) </Link>
+                </header>
+            </div>
+            <main>
+                <div>
+                    Drag and Drop Here
+                </div>
+            </main>
         </div>
+        
     )
 
 }
