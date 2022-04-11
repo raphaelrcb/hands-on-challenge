6f11a0ef3e78c37bde70eef1cbe09e7ab15064b9
Raphael <raphaelrcbarbosa@gmail.com>
2022-04-11 03:35:53 -0300

    Created filed list for cards

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Card from "components/Card/Card";
 import "./List.css"
 
-const List = () => {
-    
+export default function List( { data } ){
     return (
-        <div>
+        <div className="list-box">
+            <header>
+                <h2>{data.title}</h2>
+            </header>
+            <ul>
+                { data.lead? data.lead.map( card => <Card key={card.name} data={card} />) : "" }
+                {/* {data.lead? <Card />:""} */}
+            </ul>
         </div>
     );
 };
 
-export default List;
+// export default List;   
