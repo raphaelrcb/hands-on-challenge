b70a8cfc3b9e3984ebf18aab8a9c126901cc62cb
Raphael <raphaelrcbarbosa@gmail.com>
2022-04-11 17:38:44 -0300

    Drag function in Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,27 @@
+import UIContainer from "components/UI/Container/Container";
 import React from "react";
+import { useDrag } from "react-dnd";
 import "./Card.css"
 
-const Card = ({data}) => {
+export default function Card ({data}) {
+
+    const [{ isDragging }, dragRef] = useDrag({
+        type: 'CARD', 
+        item: {id: data.name, status: data.status},
+        collect: monitor => ({
+          isDragging: monitor.isDragging(),
+        }),
+      });
+  
     
     return (
-        <div className="card-box">
-            <header>
-                {/* <label>{data.values.leadName}</label> */}
-            </header>
-            <p>{data.name}</p>
+        <div>
+            <div className="card-box"  ref={dragRef}>
+                <header>
+                    {/* <label>{data.values.leadName}</label> */}
+                </header>
+                <p>{data.name}</p>
+            </div>
         </div>
     );
 };
-
-export default Card;
