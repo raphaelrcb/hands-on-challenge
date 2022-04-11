28c608c8362a19d3cb3ac6bf72712593478b6214
Raphael <raphaelrcbarbosa@gmail.com>
2022-04-11 03:35:04 -0300

    Created Board to Leads Panel

--- src/components/Board/Boards.js
+++ src/components/Board/Boards.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import List from "components/List/List";
 import "./Board.css"
 
-const Board = () => {
-    
+function loadCards(){
+
+    var values = []
+    var key = Object.keys(localStorage)
+    var i = 0;
+    while (i < key.length) {    
+        values.push(JSON.parse(localStorage.getItem("lead_"+i)))
+        i++;   
+    }
+    console.log(Object.values(values))    
+    return Object.values(values);  
+}
+
+function loadLists() {
+    const cards = loadCards(); 
+     return [
+        {title: 'Cliente em Potencial', lead: cards.flat() },
+        {title: 'Dados Confirmados', lead: ''}, 
+        {title: 'Reunião agendada', lead: ''}
+    ];
+} 
+
+const lists = loadLists(); 
+export default function Board({}) {
+
     return (
-        <div>
+        <div className="board-box">
+            {lists.map(list => <List key={list.title} data={list} />)}
         </div>
     );
 };
 
-export default Board;
+// export default Board;
